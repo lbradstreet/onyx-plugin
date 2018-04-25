a823a49354aa07389a742152333aaaea6dbdc082
Joaquim Torres <quimrstorres@gmail.com>
2018-04-25 22:05:32 +0200

    Update output plugin protocol implementations
    The previous version implemented a plugin template as it looked like in
    0.8.x. This change updates this template to use the new interfaces.
    
    In the tests, the core.async plugin is used as an input. That plugin
    also suffered relevant changes, namely it no longer supports sending
    :done as an end message. Instead it expects the channel to be closed.

--- resources/leiningen/new/onyx_plugin/medium_output_test.clj
+++ resources/leiningen/new/onyx_plugin/medium_output_test.clj
@@ -1,21 +1,20 @@
-(ns onyx.plugin.{{medium}}-output-test
+ (ns onyx.plugin.{{medium}}-output-test
   (:require [clojure.core.async :refer [chan >!! <!! close! sliding-buffer]]
             [clojure.test :refer [deftest is testing]]
-            [taoensso.timbre :refer [info]]
             [onyx.plugin.core-async]
             [onyx.plugin.{{medium}}-output]
             [onyx.api]))
 
 (def id (java.util.UUID/randomUUID))
 
-(def env-config 
-  {:onyx/id id
+(def env-config
+  {:onyx/tenancy-id id
    :zookeeper/address "127.0.0.1:2188"
    :zookeeper/server? true
    :zookeeper.server/port 2188})
 
-(def peer-config 
-  {:onyx/id id
+(def peer-config
+  {:onyx/tenancy-id id
    :zookeeper/address "127.0.0.1:2188"
    :onyx.peer/job-scheduler :onyx.job-scheduler/greedy
    :onyx.messaging.aeron/embedded-driver? true
@@ -53,7 +52,7 @@
 
 (def in-chan (chan (inc n-messages)))
 
-(def out-datasink (atom []))
+(def out-datasink (atom (list)))
 
 (defn inject-in-ch [event lifecycle]
   {:core.async/chan in-chan})
@@ -80,20 +79,18 @@
 (doseq [n (range n-messages)]
   (>!! in-chan {:n n}))
 
-(>!! in-chan :done)
+;; (>!! in-chan :done)
 (close! in-chan)
 
 (def v-peers (onyx.api/start-peers 2 peer-group))
 
-(def job-info 
+(def job-info
   (onyx.api/submit-job
     peer-config
     {:catalog catalog
      :workflow workflow
      :lifecycles lifecycles
      :task-scheduler :onyx.task-scheduler/balanced}))
-
-(info "Awaiting job completion")
 
 (onyx.api/await-job-completion peer-config (:job-id job-info))
 
